c523f9dd5e4347e1ab343a9b52c99028abd91607
GalloJunior <gallojunior@gmail.com>
2023-08-24 21:23:20 -0300

    Conclusão do exercicio 01

---
+++ Exercicio1/main.js
@@ -0,0 +1,9 @@
+function Calcular() {
+    // Variável Local
+    let numero1 = Number(document.getElementById('numero1').value);
+    let numero2 = Number(document.getElementById('numero2').value);
+    let numero3 = Number(document.getElementById('numero3').value);
+    let resultado = (numero1 + numero2) * numero3;
+    document.getElementById("resultado").innerHTML = "O resultado é " + resultado;
+    //console.log("O resultado é ", resultado);
+}
